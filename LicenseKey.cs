a5e7c17ba8e04f368b39156aedde9ba95f7b972f
Lukas Pokorny <lukas@nuane.com>
2022-06-10 10:17:42 +0200

    Read Rebex license key from REBEX_KEY environment variable

--- LicenseKey.cs
+++ LicenseKey.cs
@@ -5,13 +5,21 @@ namespace Rebex.Samples
     internal static class LicenseKey
     {
         /// <summary>
-        /// To run these samples, a Rebex components license is needed.
-        ///
-        /// Trial mode: Get your 30-day trial license key at https://www.rebex.net/support/trial-key.aspx
-        ///
-        /// Full mode: Purchase a Rebex component at https://www.rebex.net/
-        /// Get your full license key (see https://www.rebex.net/kb/license-keys/ for details).
+        /// If you have already purchased a Rebex component license at https://www.rebex.net/,
+        /// put your Rebex license key below. See https://www.rebex.net/kb/license-keys/ for
+        /// instructions on getting your key.
+        /// Otherwise, to start your 30-day evaluation period, get your trial key
+        /// from https://www.rebex.net/support/trial/
         /// </summary>
-        public const string Value = "==PUT_YOUR_TRIAL_KEY_HERE==";
+        public static string Value
+        {
+            get
+            {
+                string key = null; // put your license key here
+
+                // if no key is set, try reading it from REBEX_KEY environment variable.
+                return key ?? Environment.GetEnvironmentVariable("REBEX_KEY");
+            }
+        }
     }
 }
